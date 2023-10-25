77bd2df23f5279f36c8c7433f868a7f3b2d63357
Kubyshev <117076814+Kubyshev@users.noreply.github.com>
2023-10-25 20:39:50 +0300

    Add files via upload

--- 4/main.py
+++ 4/main.py
@@ -1,3 +1,5 @@
+import re
+
 import numpy as np
 import random
 import pandas as pd
@@ -5,12 +7,19 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
-df = pd.read_csv('Раздаточный материал ПЗ-5.csv')
-df.drop(labels=[df.columns[0]], axis=1, inplace=True)
+
+def ip_filter(string):
+    if bool(re.search("^(?:(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])\.){3}(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])$", string)):
+        return int(''.join([bin(int(x)+256)[3:] for x in string.split('.')]),2)
+    return string
+
+df = pd.read_csv('../NF-UQ-NIDS-v2 250000.csv')
+df.drop(labels=[df.columns[0],"Attack","Dataset"], axis=1, inplace=True)
 df_sklearn = df.copy()
-for i in range(1,29):
-    column = "A-"+str(i)
-    df_sklearn[column] = MinMaxScaler().fit_transform(np.array(df_sklearn[column]).reshape(-1, 1))
+columns = []
+[("ADDR" in x) and columns.append(x) for x in df.columns]
+for column in columns:
+    df_sklearn[column] = df_sklearn[column].apply(ip_filter)
 train, test = train_test_split(df_sklearn, test_size= 0.5 )
 
 def sig(net):
@@ -19,16 +28,17 @@ def sig_der(x):
     return sig(x)*(1-sig(x))
 def scalar_product(_str, k, k0):
     str = _str.copy()
-    str.pop("S")
+    str.pop("Label")
     metka = sum([x*y for x,y in zip(str,k)])+k0
     return metka
+
 
 m_precision_l=[]
 m_Fscore_l=[]
 m_recall_l=[]
 k=[0 for _ in range(len(df.columns)-1)]
 k0 = random.uniform(-1,1)
-for i in range(10):
+for i in range(3):
     l=[]
     m=[]
 
@@ -41,7 +51,7 @@ for i in range(10):
     for index, row in train.iterrows():
         sp = scalar_product(row, k, k0)
         label = np.round(sig(sp))
-        result = row['S']
+        result = row['Label']
 
         # error: 0, -1, 1
         error=[-(result-label)*norma*x*sig_der(sp) for x in row]
@@ -58,6 +68,8 @@ for i in range(10):
                 fn += 1
             if (label == 1) and (result == 0):
                 fp += 1
+        if ((tp+fp+tn+fn)%1000)==0:
+            print(tp+fp+tn+fn)
         k = [k-x for k,x in zip(k,error)]
         k0-=error_k0
     precision_l = tp / (tp + fp)
@@ -66,15 +78,10 @@ for i in range(10):
         print('')
     Fscore_l = (((1 + b) ** 2) * tp) / (((1 + b) ** 2) * tp + (b ** 2) * fn + fp)
     print("Коэфф.",k0,*k)
-    print("result",result)
-    print("result_",l)
-    print("fp,fn,tp,tn",fp,fn,tp,tn)
     print("precision",precision_l)
     print("recall",recall_l)
     print("Fscore",Fscore_l)
     m_precision_l.append(precision_l)
-    m_recall_l.append(precision_l)
-    m_Fscore_l.append(precision_l)
 print(*["="*100]*3,sep='\n')
 
 
@@ -87,7 +94,7 @@ b=1
 for index, row in test.iterrows():
     sp = scalar_product(row, k, k0)
     label = np.round(sig(sp))
-    result = row['S']
+    result = row['Label']
     # error: 0, -1, 1
     l.append(label)
     if (label == result):
@@ -105,9 +112,6 @@ recall_l = tp / (tp + fn)
 if (((1 + b) ^ 2) * tp + (b ^ 2) * fn + fp)==0:
     print('')
 Fscore_l = (((1 + b) ** 2) * tp) / (((1 + b) ** 2) * tp + (b ** 2) * fn + fp)
-print("result",result)
-print("result_",l)
-print("fp,fn,tp,tn",fp,fn,tp,tn)
 print("precision",precision_l)
 print("recall",recall_l)
 print("Fscore",Fscore_l)
@@ -121,18 +125,3 @@ plt.xlabel("Эпохи")
 plt.ylabel("precision")
 plt.show()
 
-newDF1=pd.DataFrame({'recall1':m_recall_l})
-plt.figure(2)
-sns.lineplot(data=newDF1)
-plt.title("Оценка recall")
-plt.xlabel("Эпохи")
-plt.ylabel("recall")
-plt.show()
-
-newDF2=pd.DataFrame({'Fscore1':m_Fscore_l})
-plt.figure(3)
-sns.lineplot(data=newDF2)
-plt.title("Оценка Fscore")
-plt.xlabel("Эпохи")
-plt.ylabel("Fscore")
-plt.show()
